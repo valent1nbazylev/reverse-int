f76b46935c39028f0adbae442e019a2f11be2422
valent1n <valent1n.bazylev@gmail.com>
2022-01-29 10:39:50 +0300

    reverse int done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function reverse (n) {
-  
+    n= Math.abs(n)
+    arr = n.toString().split('').reverse().join('')
+    
+    console.log(arr)
+    let res = Number(arr)
+return res    
 }
+
+
+/*function reverse(n){
+
+console.log(n)
+n= Math.abs(n)
+let res 
+let arr2 = []
+arr3 = []
+console.log(n)
+arr = n.toString().split('').reverse().join('')
+console.log(arr)
+
+res = Number(arr2)
+}
+
+
+
+reverse(170)*/
